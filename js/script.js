3b5474a8a05431597f46c1d037db83527dcd5ba5
jordi_brunet_espuela <jordi.brunetespuela@gmail.com>
2023-03-20 14:52:27 -0400

    Our story added and javascript on Services

--- js/script.js
+++ js/script.js
@@ -1,8 +1,27 @@
 const toggleButton = document.getElementsByClassName('toggle-btn')[0]
 const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 
+const buttonServices = document.querySelector(".display");
+const buttonCloseServices = document.querySelector(".btn-secondaryb");
+const hiddenDiv = document.querySelector(".hidden");
+
+console.log(hiddenDiv);
+
 
 toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active')
 })
 
+buttonServices.addEventListener("click", function(){
+    hiddenDiv.classList.add("show-hidden");
+    buttonServices.classList.add("hide");
+    buttonCloseServices.classList.add("show");
+
+})
+
+buttonCloseServices.addEventListener("click", function(){
+    hiddenDiv.classList.remove("show-hidden");
+    buttonServices.classList.remove("hide");
+    buttonCloseServices.classList.remove("show");
+
+})
