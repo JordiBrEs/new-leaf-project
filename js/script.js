9b77a81ddbbc099a02e9f4544b90c3c0917bcffb
jordi_brunet_espuela <jordi.brunetespuela@gmail.com>
2023-03-22 19:30:41 -0400

    JavaScript on the gallery works

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
-const hamburguer = document.querySelector(".hamburguer");
-const navMenu = document.querySelector(".nav-menu");
+// navbar hamburguer
+const hamburguer = document.querySelector('.hamburguer');
+const navMenu = document.querySelector('.nav-menu');
 
 hamburguer.addEventListener("click", () => {
     hamburguer.classList.toggle("active");
@@ -11,6 +12,7 @@ document.querySelectorAll(".nav-link").forEach(n => n. addEventListener("click",
     navMenu.classList.remove("active");
 }))
 
+// Services button
 const buttonServices = document.querySelector(".display");
 const buttonCloseServices = document.querySelector(".btn-secondaryb");
 const hiddenDiv = document.querySelector(".hidden");
@@ -31,6 +33,8 @@ buttonCloseServices.addEventListener("click", function(){
 
 })
 
+
+// Gallery button
 const buttonGallery = document.querySelector(".display2");
 const buttonShowLessGallery = document.querySelector(".btn-secondaryc");
 const hiddenDiv2 = document.querySelector(".hidden2");
@@ -46,4 +50,54 @@ buttonShowLessGallery.addEventListener("click", function(){
     hiddenDiv2.classList.remove("show-hidden");
     buttonGallery.classList.remove("hide");
     buttonShowLessGallery.classList.remove("show");
+})
+
+// Gallery functions
+
+const images = [...document.querySelectorAll('.image')];
+//  popup
+const popup = document.querySelector('.popup');
+const closeButton = document.querySelector('.close-btn');
+const imageName = document.querySelector('.image-name');
+const largeImage = document.querySelector('.large-image');
+const imageIndex = document.querySelector('.index');
+const arrowRight = document.querySelector('.right-arrow');
+const arrowLeft = document.querySelector('.left-arrow');
+
+let index = 0;
+
+images.forEach((item, i) => {
+    item.addEventListener('click', () => {
+        updateImage(i);
+        popup.classList.toggle('active');
+        
+
+    })
+})
+
+const updateImage = (i) => {
+    let path = `img/job${i+1}.png`;
+    largeImage.src = path;
+    imageName.innerHTML = `Our job #${i+1}`;
+    index = i;
+
+}
+
+
+closeButton.addEventListener("click", () => {
+    popup.classList.toggle('active');
+
+})
+
+
+arrowLeft.addEventListener("click", () => {
+    if(index > 0){
+        updateImage(index - 1);
+    }
+})
+
+arrowRight.addEventListener("click", () => {
+    if(index < images.length -1){
+        updateImage(index + 1);
+    }
 })
